b32f176739c281a4f7dd75af567ebc5d7f9ba22d
Noel_Niko <noel.nosse@grainger.com>
2023-02-11 22:32:42 -0500

    feature/Jokes-with-navigation # Work on rotation

--- app/src/main/java/com/livingtechusa/gotjokes/ui/display/DisplayScreen.kt
+++ app/src/main/java/com/livingtechusa/gotjokes/ui/display/DisplayScreen.kt
@@ -3,18 +3,23 @@ package com.livingtechusa.gotjokes.ui.display
 import android.net.Uri
 import android.util.Log
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTransformGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Button
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
@@ -23,7 +28,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalLifecycleOwner
@@ -39,24 +47,10 @@ import com.livingtechusa.gotjokes.R
 import com.livingtechusa.gotjokes.ui.build.BuildEvent
 import com.livingtechusa.gotjokes.ui.build.BuildViewModel
 import com.livingtechusa.gotjokes.ui.components.DisplayImgCard
+import com.livingtechusa.gotjokes.ui.components.animation_utils.MultiTouchBox
 import com.livingtechusa.gotjokes.util.TakeScreenShot
 import com.livingtechusa.gotjokes.util.findActivity
 import kotlin.math.roundToInt
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectTransformGestures
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.TransformOrigin
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.pointerInput
-import com.livingtechusa.gotjokes.ui.components.animation_utils.MultiTouchBox
-import java.lang.Math.PI
-import java.lang.Math.cos
-import java.lang.Math.sin
 
 @Composable
 fun DisplayScreen() {
@@ -102,97 +96,133 @@ fun DisplayScreen() {
         mutableStateOf((height * .025).toInt())
     }
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        LazyColumn(
+    fun Offset.rotateBy(angle: Float): Offset {
+        val angleInRadians = angle * Math.PI / 180
+        return Offset(
+            (x * Math.cos(angleInRadians) - y * Math.sin(angleInRadians)).toFloat(),
+            (x * Math.sin(angleInRadians) + y * Math.cos(angleInRadians)).toFloat()
+        )
+    }
+
+    var offset = remember { mutableStateOf(Offset.Zero) }
+    var zoom = remember { mutableStateOf(1f) }
+    var angle = remember { mutableStateOf(0f) }
+
+    BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+
+        Button(
+           // modifier = Modifier.align(Alignment.CenterHorizontally),
+            onClick = {
+                try {
+
+                    val imageUri = TakeScreenShot.takeScreenShot(
+                        activity.window.decorView,
+                        caption,
+                        (height * 2).dp,
+                        width.dp
+                    )
+                    val uri: Uri = imageUri
+
+                    buildViewModel.onTriggerEvent(BuildEvent.Save(uri))
+                    Toast.makeText(activity, "Saved", Toast.LENGTH_SHORT).show()
+                } catch (e: Exception) {
+                    val TAG = "ScreenShot"
+                    Log.e(
+                        TAG,
+                        "Error message: " + e.message + " with cause " + e.cause
+                    )
+                    Toast.makeText(
+                        activity,
+                        "Unable to save. \n Error: " + e.cause,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        ) {
+            Text(stringResource(R.string.save))
+        }
+        Column(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxHeight((height / 5) * 3.5f)
         ) {
             if (image == null) {
-                item {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(25.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        CircularProgressIndicator()
-                    }
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(25.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    CircularProgressIndicator()
                 }
+
             } else {
-                item {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(8.dp)
-                    ) {
-
-                        Spacer(
-                            Modifier.height((height / 5).dp)
-                        )
-                        if (image != null) {
-                            DisplayImgCard(url = image!!)
-                        }
-                        Spacer(modifier = Modifier.height(8.dp))
-                        Text(
-                            modifier = Modifier
-                                .padding(55.dp, 0.dp, 55.dp, 0.dp)
-                                .align(Alignment.CenterHorizontally)
-                                .offset() {
-                                    IntOffset(
-                                        x = offSetX.value.roundToInt(),
-                                        y = offSetY.value.roundToInt()
-                                    )
-                                }
-                                .pointerInput(Unit) {
-                                    detectDragGestures { change, dragAmount ->
-                                        change.consumeAllChanges()
-                                        offSetX.value += dragAmount.x
-                                        offSetY.value += dragAmount.y
-                                    }
-                                }
-                                .clickable {
-                                    buildViewModel.onTriggerEvent(BuildEvent.UpdateColor)
-                                },
-                            text = caption,
-                            fontSize = fontSize.value.sp,
-                            color = textColor
-                        )
-                        Spacer(modifier = Modifier.height(16.dp))
-
-                        Button(
-                            modifier = Modifier.align(Alignment.CenterHorizontally),
-                            onClick = {
-                                try {
-
-                                    val imageUri = TakeScreenShot.takeScreenShot(
-                                        activity.window.decorView,
-                                        caption,
-                                        (height * 2).dp,
-                                        width.dp
-                                    )
-                                    val uri: Uri = imageUri
-
-                                    buildViewModel.onTriggerEvent(BuildEvent.Save(uri))
-                                    Toast.makeText(activity, "Saved", Toast.LENGTH_SHORT).show()
-                                } catch (e: Exception) {
-                                    val TAG = "ScreenShot"
-                                    Log.e(
-                                        TAG,
-                                        "Error message: " + e.message + " with cause " + e.cause
-                                    )
-                                    Toast.makeText(
-                                        activity,
-                                        "Unable to save. \n Error: " + e.cause,
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                }
-                            }
-                        ) {
-                            Text(stringResource(R.string.save))
-                        }
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp)
+                ) {
+                    Spacer(
+                        Modifier.height((height / 5).dp)
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    if (image != null) {
+                        DisplayImgCard(url = image!!)
                     }
                 }
             }
+        }
+        Box() {
+            Text(
+                modifier = Modifier
+                    .padding(55.dp, 0.dp, 55.dp, 0.dp)
+                    //.align(Alignment.CenterHorizontally)
+                    .offset() {
+                        IntOffset(
+                            x = offSetX.value.roundToInt(),
+                            y = offSetY.value.roundToInt()
+                        )
+                    }
+                    .pointerInput(Unit) {
+                        detectTransformGestures(
+                            panZoomLock = false,
+                            onGesture = { centroid, pan, gestureZoom, gestureRotate ->
+                                val oldScale = zoom
+                                val newScale = (zoom.value * gestureZoom)
+
+                                // For natural zooming and rotating, the centroid of the gesture should
+                                // be the fixed point where zooming and rotating occurs.
+                                // We compute where the centroid was (in the pre-transformed coordinate
+                                // space), and then compute where it will be after this delta.
+                                // We then compute what the new offset should be to keep the centroid
+                                // visually stationary for rotating and zooming, and also apply the pan.
+                                offset.value =
+                                    (offset.value + centroid / oldScale.value).rotateBy(
+                                        gestureRotate
+                                    ) -
+                                            (centroid / newScale + pan / oldScale.value)
+                                zoom.value = newScale
+                                angle.value += gestureRotate
+                            }
+                        )
+                    }
+                    .graphicsLayer {
+                        translationX = -offset.value.x * zoom.value
+                        translationY = -offset.value.y * zoom.value
+                        scaleX = zoom.value
+                        scaleY = zoom.value
+                        rotationZ = angle.value
+                        transformOrigin = TransformOrigin(0f, 0f)
+                    }
+                    .background(Color.Transparent)
+//                .fillMaxSize()
+                    .clickable {
+                        buildViewModel.onTriggerEvent(BuildEvent.UpdateColor)
+                    }
+                    .fillMaxSize(),
+                text = caption,
+                fontSize = fontSize.value.sp,
+                color = textColor
+            )
         }
     }
 }
